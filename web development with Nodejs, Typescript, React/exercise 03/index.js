55b16670219ce3929d3434280bdb0c6e7177eed6
nalchevanidze <d.nalchevanidze@gmail.com>
2018-10-20 18:11:06 +0200

    exercise 03

--- web development with Nodejs, Typescript, React/exercise 03/index.js
+++ web development with Nodejs, Typescript, React/exercise 03/index.js
@@ -1,46 +1,59 @@
-const http = require('http');
 const express = require('express');
-
 const app = express();
 const path = require("path");
 const fs = require("fs");
 
-app.use(cors());
+/**
+ * TODO: 3.1 write success and Fail page
+ * TODO: 3.2 find some js specific failure, why is it security risk?
+ */
 
+app.get("/admin", function (req, res) {
+    const filePath = path.join(__dirname, "tocken.txt");
+
+    /**
+     * TODO: is it good idea to use syncronious readFile? why is it Bad?
+     */
+    const Secrettocken = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    const inputTocken = req.query.tocken;
+    const redirectPath = inputTocken == Secrettocken ? "/sucsses" : "/fail";
+
+    res.status(201);
+    res.redirect(redirectPath)
+});
+
+
+app.get("/sucsses", function (req, res) {
+    res.send("you are admin")
+});
+
+app.get("/fail", function (req, res) {
+    res.send("something gone wrong")
+});
+
+
+/**
+ * TODO: 3.3 on every doamin: /files/job-01 returns text from files/job-01.txt
+ */
 app.get("/files/*", function (req, res) {
+
     const filePath = path.join(__dirname + req.path)
-    console.log("opens file: " + req.path + "in : " + filePath);
     res.sendFile(filePath);
-});
-
-app.get("/icon", function (req, res) {
-    const ids = req.query.id;
-    console.log("get Icon", ids);
-    const filePath = path.join(__dirname, "/assets/", "icons.json");
-    const list = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    const result = {};
-    ids.forEach(id => {
-        const name = "src.icons." + id;
-        result[id] = list[name] || {
-            is: "empty"
-        };
-    })
-    res.json(result);
 
 });
 
+/**
+ * TODO: 3.4 in returns page 404 with text page not found and header 404
+ */
 app.get("/*", function (req, res) {
-    console.log(req.headers.host);
-    const filePath = path.join(__dirname, "/assets/", req.path);
-    console.log("opens file: " + req.path + "in : " + filePath);
-    res.sendFile(filePath);
+    const message = "Page not found"
+
+    res.status(404);
+    res.setHeader("title", message)
+    res.send(message)
 });
 
-const httpServer = http.createServer(app);
-const httpServerInstance = httpServer.listen(8080);
 
-process.on("exit", function () {
-    console.log("close Server");
-    httpsServerInstance.close();
-    httpServerInstance.close();
-})
+
+app.listen(3000);
