97167e7bd3f42cab4494c6db21d19da9bb65d4b8
nalchevanidze <d.nalchevanidze@gmail.com>
2018-10-20 21:09:34 +0200

    set exercise 5

--- nodejs-intro/2.express-fetch/exercise-05.js
+++ nodejs-intro/2.express-fetch/exercise-05.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fetch = require("fetch");
+const fetch = require("node-fetch");
 const {
     read,
     write
@@ -9,9 +9,9 @@ const {
 app.get("/success", (_, res) => res.send("you are admin"));
 app.get("/fail", (_, res) => res.send("something gone wrong"));
 
-/** 
- * http://localhost:3000/admin?tocken=1234567890&user=dato 
- * 
+/**
+ * http://localhost:3000/admin?tocken=1234567890&user=dato
+ *
  */
 app.get("/bookmark-service", async ({
     query: {
@@ -20,24 +20,27 @@ app.get("/bookmark-service", async ({
 }, res) => {
     try {
         if (!user) {
-            throw new Error("no user to check")
+            throw new Error("no user to check");
         }
 
         const register = JSON.parse(await read("./register.json"));
 
         if (register.find(x => x === user)) {
-            throw new Error("already registered")
+            throw new Error("already registered");
         }
 
         const newRegister = [...register, user];
         await write("./register.json", JSON.stringify(newRegister, null, 2));
-        res.status(201);
-        res.send("success")
+
+        res.status(201).send({
+            success: "just registered"
+        });
     } catch (err) {
-        res.status(404);
-        res.send(`STOP: you have ${err.message}`);
+        res.status(403).send({
+            error: err.message
+        });
     }
-})
+});
 
 app.get("/register", async ({
     query: {
@@ -45,27 +48,32 @@ app.get("/register", async ({
         tocken
     }
 }, res) => {
-
     try {
-
         if (!tocken || !user) {
-            throw new Error("no aozhorisation data")
+            throw new Error("no aozhorisation data");
         }
 
         const secret = await read("./tocken.txt");
 
         if (tocken != secret) {
-            throw new Error("invalid Tocken")
-        };
+            throw new Error("invalid Tocken");
+        }
 
-        const bookmarkIt = await fetch("/bookmark-service")
+        const {
+            error
+        } = await fetch(`http://localhost:3000/bookmark-service?user=?${user}`).then(res => res.json());
 
+        if (error) {
+            throw new Error(error);
+        }
+
+        res.status(201);
+        res.redirect("/success");
     } catch (err) {
-        console.error(`STOP: you have ${err.message}`);
+        console.error(`STOP: ${err.message}`);
         res.status(404);
         res.redirect("/fail");
     }
 });
-
 
 app.listen(3000);
