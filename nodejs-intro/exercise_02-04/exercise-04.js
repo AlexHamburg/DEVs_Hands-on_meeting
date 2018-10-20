f1a2a761adeddf169fada51653237be05bf5ee20
nalchevanidze <d.nalchevanidze@gmail.com>
2018-10-20 19:16:03 +0200

    simple register

--- nodejs-intro/exercise_02-04/exercise-04.js
+++ nodejs-intro/exercise_02-04/exercise-04.js
@@ -2,29 +2,76 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const {
-    readFile
+    readFile,
+    writeFile
 } = require("fs");
 
-app.get("/sucsses", (_, res) => res.send("you are admin"));
-
+app.get("/success", (_, res) => res.send("you are admin"));
 app.get("/fail", (_, res) => res.send("something gone wrong"));
 
-app.get("/admin", function (req, res) {
+/** 
+ * http://localhost:3000/admin?tocken=1234567890&user=dato 
+ * 
+ */
+
+app.get("/register", function (req, res) {
+
+    const handleError = () => {
+        res.status(404);
+        return res.redirect("/fail");
+    }
+
+    const {
+        tocken,
+        user
+    } = req.query;
+
+    if (!tocken || !user) {
+        return handleError();
+    }
     const filePath = path.join(__dirname, "tocken.txt");
 
-    readFile(filePath, 'utf8', (error, file) => {
+    readFile(filePath, 'utf8', (error, tockenTXT) => {
+        if (error) {
+            return handleError();
+        }
 
-        const Secrettocken = JSON.parse(file);
+        const Secrettocken = JSON.parse(tockenTXT);
 
-        const inputTocken = req.query.tocken;
-        const redirectPath = inputTocken == Secrettocken ? "/sucsses" : "/fail";
-        console.log(redirectPath, inputTocken);
-        res.status(201);
-        res.redirect(redirectPath)
+        const rightTocken = tocken == Secrettocken;
+
+        if (!rightTocken) {
+            return handleError();
+        }
+
+        readFile(path.join(__dirname, "register.json"), 'utf8', (error, registerJson) => {
+            if (error) {
+                return handleError();
+            }
+
+            const register = JSON.parse(registerJson);
+            const isAlreadyRegistered = register.find(x => x === user);
+
+            console.log(register, isAlreadyRegistered);
+
+            if (isAlreadyRegistered) {
+                return handleError();
+            }
+
+            let newRegister = JSON.stringify([...register, user], null, 2);
+
+            writeFile("./register.json", newRegister, (error) => {
+
+                if (error) {
+                    return handleError();
+                }
+
+                res.status(201);
+                res.redirect("/success")
+
+            });
+        })
     });
 });
-
-
-
 
 app.listen(3000);
