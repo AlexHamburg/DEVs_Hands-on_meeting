ef34227f33ee6a2c109331872628f51a25e296de
nalchevanidze <d.nalchevanidze@gmail.com>
2018-10-20 18:14:40 +0200

    exercose 3 todos

--- web development with Nodejs, Typescript, React/exercise 03/index.js
+++ web development with Nodejs, Typescript, React/exercise 03/index.js
@@ -3,19 +3,18 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 
-/**
- * TODO: 3.1 write success and Fail page
- * TODO: 3.2 find some js specific failure, why is it security risk?
- */
-
 app.get("/admin", function (req, res) {
     const filePath = path.join(__dirname, "tocken.txt");
 
     /**
-     * TODO: is it good idea to use syncronious readFile? why is it Bad?
+     * TODO: 3.1 is it good idea to use syncronious readFile? why is it Bad?
      */
     const Secrettocken = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
+    /**
+     * TODO: 3.2 find some js specific failure, why is it security risk?
+     *  what if tocken is empty?
+     */
     const inputTocken = req.query.tocken;
     const redirectPath = inputTocken == Secrettocken ? "/sucsses" : "/fail";
 
@@ -24,17 +23,23 @@ app.get("/admin", function (req, res) {
 });
 
 
+/** 
+ *  TODO: 3.3 write success and Fail page
+ */
 app.get("/sucsses", function (req, res) {
     res.send("you are admin")
 });
 
+/** 
+ *  TODO: 3.4 write success and Fail page
+ */
 app.get("/fail", function (req, res) {
     res.send("something gone wrong")
 });
 
 
 /**
- * TODO: 3.3 on every doamin: /files/job-01 returns text from files/job-01.txt
+ * TODO: 3.5 on every doamin: /files/job-01 returns text from files/job-01.txt
  */
 app.get("/files/*", function (req, res) {
 
@@ -44,7 +49,7 @@ app.get("/files/*", function (req, res) {
 });
 
 /**
- * TODO: 3.4 in returns page 404 with text page not found and header 404
+ * TODO: 3.6 in returns page 404 with text page not found and header 404
  */
 app.get("/*", function (req, res) {
     const message = "Page not found"
@@ -53,7 +58,5 @@ app.get("/*", function (req, res) {
     res.setHeader("title", message)
     res.send(message)
 });
-
-
 
 app.listen(3000);
