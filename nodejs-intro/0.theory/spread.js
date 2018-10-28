26e585cb065aa130bddecbddb15c7718d67d95f5
nalchevanidze <d.nalchevanidze@gmail.com>
2018-10-28 11:53:22 +0100

    spread

--- nodejs-intro/0.theory/spread.js
+++ nodejs-intro/0.theory/spread.js
@@ -1,3 +1,4 @@
+// CASE: OBJECT
 const movePoint = ({
     x,
     y = 0,
@@ -15,3 +16,12 @@ const p1 = {
 p2 = movePoint(p1)
 
 console.log(p1, p2);
+
+// CASE: ARRAY
+
+const a1 = [1, 2, 3, 4, 5, 6];
+const a2 = ["A", "B", "C"];
+
+let [first, second, ...rest] = [...a1, ...a2];
+
+console.log(first, second, rest);
