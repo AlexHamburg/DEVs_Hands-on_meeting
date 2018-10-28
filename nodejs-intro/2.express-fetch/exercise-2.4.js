850ec70fc31f4a0e7082d1ac956eb98866f84113
nalchevanidze <d.nalchevanidze@gmail.com>
2018-10-28 17:31:40 +0100

    exercise 2.4 solution

--- nodejs-intro/2.express-fetch/exercise-2.4.js
+++ nodejs-intro/2.express-fetch/exercise-2.4.js
@@ -1,29 +1,33 @@
 const express = require("express");
 const app = express();
 const {
-    write
+    write,
+    read
 } = require("./myIO");
-/**
- * TODO: write bookmark service on endpoint: http://localhost:8080/bookmark-service
- */
 
-app.get( /** TODO: 1 */ "<your Path>", (req, res) => {
+const path = "./register.json";
+
+app.get("/bookmark-service", async (req, res) => {
+
+    const {
+        user
+    } = req.query;
 
     try {
 
-        if ( /* TODO: 2 if username dont exists*/ ) {
+        if (!user) {
             throw new Error("no user to check");
         }
 
-        const register = JSON.parse( /** TODO: 3 read file */ );
+        const register = JSON.parse(await read(path));
 
         if (register.find(x => x === user)) {
             throw new Error("already registered");
         }
 
-        await write("./register.json", JSON.stringify( /* TODO: 4 updated user list  */ ));
+        await write("./register.json", JSON.stringify([...register, user]));
 
-        res.status(201).send( /** succes message */ );
+        res.status(201).send("success");
     } catch (err) {
         res.status(403).send({
             error: err.message
@@ -31,4 +35,4 @@ app.get( /** TODO: 1 */ "<your Path>", (req, res) => {
     }
 });
 
-app.listen( /* TODO: 5 set port */ );
+app.listen(8080);
